ac5e82af9fc763918d92d342cd33452fa2298bfd
tgmeng <tgmeng@163.com>
2017-12-26 14:10:33 +0800

    Initial Commit

---
+++ Movse/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Movse
+//
+//  Created by tgmeng on 2017/12/26.
+//  Copyright © 2017年 lazyfabric. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Movse/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Movse
+//
+//  Created by tgmeng on 2017/12/26.
+//  Copyright © 2017年 lazyfabric. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
